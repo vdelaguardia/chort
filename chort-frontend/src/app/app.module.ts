6c9eb65f2f5be7cfc97ecbcb1f1e2473277d9c6d
vdelaguardia <victoriadelaguardia@yahoo.com>
2022-06-15 22:08:24 -0500

    added auth0 authentication and login page

--- chort-frontend/src/app/app.module.ts
+++ chort-frontend/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthButtonComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-4uemafmy.auth0.com',
+      clientId: 'Q35vfJGwx76UorQAl3PHCRJw0MoSEt51'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ chort-frontend/src/app/auth-button/auth-button.component.ts
@@ -0,0 +1,17 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-auth-button',
+  templateUrl: './auth-button.component.html',
+  styleUrls: ['./auth-button.component.less']
+})
+export class AuthButtonComponent implements OnInit {
+
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+}
