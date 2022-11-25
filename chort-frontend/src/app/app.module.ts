23e9c71b08c3374d6176150f39f067f9322a33e2
Victoria de la Guardia <victoriadelaguardia@yahoo.com>
2022-11-25 15:36:19 -0600

    implemented material buttons, updated packages, updated logo img

--- chort-frontend/src/app/app.module.ts
+++ chort-frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 
@@ -29,6 +30,7 @@ import { HomeComponent } from './home/home.component';
     ]),
     BrowserAnimationsModule,
     MatButtonModule,
+    MatInputModule,
     FormsModule
   ],
   providers: [],
--- chort-frontend/src/app/login/login.component.ts
+++ chort-frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Email } from '../models/email';
 
 @Component({
   selector: 'app-login',
@@ -6,14 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
+  email: Email;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.email.invalid = true;
   }
 
   onLoginSubmit(): void {
     console.log('login submitted');
   }
 
+  getErrorMessage(field: string): string{
+    if (field === 'email' && this.email.invalid) {
+      return 'Invalid email address. Please enter a valid email.';
+    }
+  }
 }
